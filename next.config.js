3c53297e33e45c370fe99b57dbb29f37ee3b4050
Matthieu Clay <matthieu.clay@outlook.fr>
2022-03-15 09:48:48 +0100

    :wrench: add images domains

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: [
+      "s3.us-east-2.amazonaws.com",
+      "nomics-api.s3.us-east-2.amazonaws.com",
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
